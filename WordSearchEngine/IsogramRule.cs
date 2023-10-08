6b97c67e0603b2cc4afb9a18413f58a581146264
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 01:04:18 +0200

    Initialized isogram rule

---
+++ WordSearchEngine/IsogramRule.cs
@@ -0,0 +1,8 @@
+﻿namespace WordSearchEngine;
+
+public class IsogramRule
+{
+    public List<string> ApplyRule(List<string> words) => words.Where(word => IsIsogram(word)).ToList();
+
+    private bool IsIsogram(string word) => false; //TODO: Implement isogram check logic
+}
