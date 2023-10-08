40f061dd1cb51608cd7f2cf1d94e77000b210727
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-08 23:46:06 +0200

    added Palindrome rule

---
+++ WordSearchEngine/PalindromeRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearchEngine
+{
+    public class PalindromeRule
+    {
+        public List<string> ApplyRule(List<string> words) => words.Where(word => IsPalindrome(word)).ToList();
+
+        private bool IsPalindrome(string word) => false; //TODO: Implement palindrome check logic
+    }
+}
