55d2d9b79d0a4741f35b79e236b487bbd3af77a6
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-08 23:55:46 +0200

    Implemented palindrome check logic

--- WordSearchEngine/PalindromeRule.cs
+++ WordSearchEngine/PalindromeRule.cs
@@ -10,6 +10,25 @@ namespace WordSearchEngine
     {
         public List<string> ApplyRule(List<string> words) => words.Where(word => IsPalindrome(word)).ToList();
 
-        private bool IsPalindrome(string word) => false; //TODO: Implement palindrome check logic
+        private bool IsPalindrome(string word)
+        {
+            // Convert the word to lowercase to ignore case
+            word = word.ToLower();
+
+            int length = word.Length;
+
+            // We only need to check half the word because the other half is the reverse of the first half in a palindrome.
+            for (int i = 0; i < length / 2; i++)
+            {
+                // Check if characters at positions i and length - i - 1 are not the same.
+                // If they are not the same, the word is not a palindrome.
+                if (word[i] != word[length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
