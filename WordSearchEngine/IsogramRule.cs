132365b5de72e24d4995b684b4c678c3cdc2766d
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 01:06:59 +0200

    Implemented Isogram check logic

--- WordSearchEngine/IsogramRule.cs
+++ WordSearchEngine/IsogramRule.cs
@@ -4,5 +4,24 @@ public class IsogramRule
 {
     public List<string> ApplyRule(List<string> words) => words.Where(word => IsIsogram(word)).ToList();
 
-    private bool IsIsogram(string word) => false; //TODO: Implement isogram check logic
+    private bool IsIsogram(string word)
+    {
+        // Convert the word to lowercase to ignore case
+        word = word.ToLower();
+
+        // Create a HashSet to store seen characters
+        var seenCharacters = new HashSet<char>();
+
+        foreach (char character in word)
+        {
+            // If the character is not a space and it's already seen, it's not an isogram
+            if (character != ' ' && seenCharacters.Contains(character))
+                return false;
+
+            // Add the character to the set
+            seenCharacters.Add(character);
+        }
+
+        return true;
+    }
 }
