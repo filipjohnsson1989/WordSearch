af7717c74ca0f6cb749f1648613eb429bffb570b
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 04:45:09 +0200

    Implemented init unit tests for MinLength

---
+++ WordSearch.Tests/MinLenghtRuleTests.cs
@@ -0,0 +1,61 @@
+﻿using Shouldly;
+
+namespace WordSearch.Tests;
+
+public class MinLengthRuleTests
+{
+    [Fact]
+    public void ApplyRule_Returns_WordsWithMinLength()
+    {
+        // Arrange
+        List<string> words = new List<string> { "Isogram", "semordnilap", "god", "dog", "deified", "repaid", "reward", "drawer", "diaper", "Palindromes" };
+        int minLength = 6;
+        MinLengthRule rule = new(minLength);
+
+        // Act
+        var result = rule.ApplyRule(words);
+
+        // Assert
+        result.Count.ShouldBe(8); // exact number of words with min length
+
+        result.ShouldContain("Isogram");
+        result.ShouldContain("semordnilap");
+        result.ShouldContain("deified");
+        result.ShouldContain("repaid");
+        result.ShouldContain("reward");
+        result.ShouldContain("drawer");
+        result.ShouldContain("diaper");
+        result.ShouldContain("Palindromes");
+
+    }
+
+    [Fact]
+    public void ApplyRule_Returns_NoWordsWithMinLength()
+    {
+        // Arrange
+        List<string> words = new List<string> { "Isogram", "semordnilap", "god", "dog", "deified", "repaid", "reward", "drawer", "diaper", "Palindromes" };
+        int minLength = 12;
+        MinLengthRule rule = new(minLength);
+
+        // Act
+        var result = rule.ApplyRule(words);
+
+        // Assert
+        result.ShouldBeEmpty();
+    }
+}
+
+public class MinLengthRule
+{
+    private readonly int _minLength;
+
+    public MinLengthRule(int minLength)
+    {
+        this._minLength = minLength;
+    }
+
+    public List<string> ApplyRule(List<string> words)
+    {
+        throw new NotImplementedException();
+    }
+}
