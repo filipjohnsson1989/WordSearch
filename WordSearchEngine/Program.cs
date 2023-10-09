68ad545d2894f171ee59589a9dfbd40965e837af
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 05:46:19 +0200

    Added Accepting Rules and Performing Word Search in Program.cs

--- WordSearchEngine/Program.cs
+++ WordSearchEngine/Program.cs
@@ -2,8 +2,25 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] rules)
     {
-        Console.WriteLine("Hello, World!");
+        if (rules.Length == 0)
+        {
+            Console.WriteLine("Please provide rules for word search.");
+            return;
+        }
+
+        // Load the word list from a file
+        var wordReader = new WordReader();
+        var words = wordReader.ReadWordsFromWordList();
+
+        var service = new WordSearchService(words);
+
+        var result = service.Search(rules);
+
+        foreach (var word in result)
+        {
+            Console.WriteLine(word);
+        }
     }
 }
