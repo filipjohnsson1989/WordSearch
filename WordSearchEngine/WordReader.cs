4ceb2534d1c2b60baaf8fde030d98afeccb34661
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 05:44:36 +0200

    Implemented WordReader for Reading Words from File

---
+++ WordSearchEngine/WordReader.cs
@@ -0,0 +1,36 @@
+﻿namespace WordSearchEngine;
+
+public class WordReader
+{
+    public List<string> ReadWordsFromWordList()
+    {
+        var words = new List<string>();
+
+        try
+        {
+            var filePath = "wordlist.txt"; // Relative path to the file
+
+            if (File.Exists(filePath))
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Add each line (word) to the list
+                foreach (var line in lines)
+                {
+                    words.Add(line.Trim());
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file 'wordlist.txt' does not exist.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while reading the wordlist: " + ex.Message);
+        }
+
+        return words;
+    }
+}
