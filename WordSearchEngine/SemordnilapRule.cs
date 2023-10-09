a6d6d82b9f1bcd67d2d4cf9979fb6b80703bb0a8
Filip Johnsson <filip.johnsson@causeywestling.com>
2023-10-09 02:48:14 +0200

    Initialized Semordnilap rule

---
+++ WordSearchEngine/SemordnilapRule.cs
@@ -0,0 +1,8 @@
+﻿namespace WordSearchEngine;
+
+public class SemordnilapRule
+{
+    public List<string> ApplyRule(List<string> words) => words.Where(word => IsSemordnilap(word, words)).ToList();
+
+    private bool IsSemordnilap(string word, List<string> words) => false; //TODO: Implement Semordnilap check logic
+}
